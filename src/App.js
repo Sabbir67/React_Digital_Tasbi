ffabd28c9ee36f37002f5eab8a0f7d62ccce5df3
Sabbir67 <sabbir.sajan67@gmail.com>
2021-09-24 21:30:01 +0600

    Digital Tasbi Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <Counter></Counter>
+    </div>
+  );
+}
+
+function Counter(){
+
+  const [count , setCount] = useState(1);
+
+  const handelIncrease = () =>{
+    const newCount = count + 1;
+    setCount(newCount);
+  }
+
+  const handelDecrease = () => {
+    if(count > 0){
+      const newDecrease = count - 1;
+      setCount(newDecrease);
+    }
+    
+  }
+
+  const resetValue = () => {
+    const resetVal = 0;
+    setCount(resetVal); 
+  }
+  return(
+    <div>
+        <h1>React Digital Tasbi</h1>
+        <h1>Count : {count} </h1>
+        <button onClick={handelIncrease}>Increase</button>
+        <button onClick={handelDecrease}>Decrease</button>
+        <button onClick={resetValue}>Reset</button>
     </div>
   );
 }
